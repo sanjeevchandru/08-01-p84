222afe7c24fc3a6f91facb34c130abb2dbdc36a8
sanjeevchandru <chandrusanjeev5@gmail.com>
2024-01-08 21:58:36 +0530

    08-01-p84

---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("48.to reverse a given string."+"<br>");
+function test48(str){
+    return str.split("").reverse().join("");
+}
+document.write("The given string is :sanjeev "+test48("sanjeev")+"<br><br>");
